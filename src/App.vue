e3c3602ce198d5ecb0c0950b18967ed4f690cf03
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-12-01 17:16:09 +0100

    struttura

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader @clickBtn = 'userSearch'/>
+    <FilmLibrary/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import AppHeader from './components/AppHeader.vue'
+import FilmLibrary from './components/FilmLibrary.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
+    FilmLibrary
+  },
+  data() {
+    return {
+      apiUrl: 'https://api.themoviedb.org/3/search/movie',
+      apiKey: '0e7cd09201aa25f0f40469f08d9be41c',
+      filmsList : [],
+      // searchedFilm : ''
+    }
+  },
+  props: {
+
+  },
+  // created() {
+  //   this.getFilm(this.apiParams);
+  // },
+  computed: {
+
+  },
+  methods: {
+    getFilm: function(apiParams) {
+      axios
+      .get(this.apiUrl,apiParams)
+      .then((result) => {
+        this.filmsList = result.data.results;
+        console.log(this.filmsList);
+
+      
+      })
+      .catch((error) => {
+        console.log('errore', error);
+      })
+       
+    },
+    userSearch (inputText) {
+      const paramsSearch= {
+        params: {
+          api_key: this.apiKey,
+          query: inputText,
+          language: 'it-IT'
+        }
+      }
+      this.getFilm(paramsSearch);
+      
+  
+
+    } 
+
   }
 }
 </script>
---
+++ src/components/FilmLibrary.vue
@@ -0,0 +1,40 @@
+<template>
+<section>
+    <Film/>
+</section>
+</template>
+
+<script>
+// import axios from 'axios'
+import Film from '../components/Film.vue'
+
+export default {
+    name:"FilmLibrary",
+    components: {
+        Film
+    },
+    data() {
+        return {
+           
+        }
+    },
+    props: {
+
+    },
+    created() {
+        
+    },
+    comupted: {
+
+    },
+    methods: {
+       
+
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
