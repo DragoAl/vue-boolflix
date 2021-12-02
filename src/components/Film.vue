fb5bb233fb46cbca81e960bfe07f56e1ec062012
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-12-02 12:54:30 +0100

    vote base 10 to base 5

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -9,7 +9,7 @@
         <div v-else><img class="flags" src='../assets/img/arco-flag.png' alt="arcobaleno">Lang= {{details.original_language}} </div>
 
 
-        <div>{{details.vote_average}}</div>
+        <div>{{voteBase5}}</div>
         
         
     </div>
@@ -22,17 +22,27 @@ export default {
         return {
             baseUrl: 'https://image.tmdb.org/t/p/',
             sizeImg:'w342/',
-            posterPath: this.details.poster_path 
+            posterPath: this.details.poster_path,
+            originalVoteAvg: this.details.vote_average,
+            voteBase5:'' 
         }
     },
     props: {
         details: Object,
     },
+    created() {
+        this.vote1to5()
+    },
+
     methods : {
-        flagImg() {
-            if (this.details.original_language === 'it') {
-                this.details.original_language = this.flagIta
-            }
+        // flagImg() {
+        //     if (this.details.original_language === 'it') {
+        //         this.details.original_language = this.flagIta
+        //     }
+        // },
+        vote1to5() {
+            console.log(this.vote =this.originalVoteAvg / 2);
+            return this.voteBase5 =Math.ceil(this.originalVoteAvg / 2)
         }
     }
 
--- src/components/TvSeries.vue
+++ src/components/TvSeries.vue
@@ -9,7 +9,7 @@
         <div v-else><img class="flags" src='../assets/img/arco-flag.png' alt="arcobaleno">Lang= {{details.original_language}} </div>
 
 
-        <div>{{details.vote_average}}</div>
+        <div >{{voteBase5}} </div>
         
         
     </div>
@@ -22,14 +22,25 @@ export default {
         return {
             baseUrl: 'https://image.tmdb.org/t/p/',
             sizeImg:'w342/',
-            posterPath: this.details.poster_path 
+            posterPath: this.details.poster_path, 
+            originalVoteAvg: this.details.vote_average,
+            voteBase5:''
+            
         }
     },
     props: {
         details: Object,
     },
-    methods : {
+    created() {
+        this.vote1to5()
 
+
+    },
+    methods : {
+        vote1to5() {
+            console.log(this.vote =this.originalVoteAvg / 2);
+            return this.voteBase5 =Math.floor(this.originalVoteAvg / 2)
+        }
     }
 
 }
