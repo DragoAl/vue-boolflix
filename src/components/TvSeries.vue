da63ae4936c7defa13e8a8479ec57b6f58d3497d
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-12-02 11:40:29 +0100

    poster tvseries

--- src/components/TvSeries.vue
+++ src/components/TvSeries.vue
@@ -1,11 +1,12 @@
 <template>
     <div id="tvseries">
+        <img :src="baseUrl + sizeImg + posterPath" :alt= details.original_title>
         <h3>{{details.original_name}}</h3>
         <h4>{{details.name}}</h4>
         
-        <div v-if="details.original_language === 'it' "><img src= '../assets/img/ita-flag.png' alt="italy"></div>
-        <div v-else-if="details.original_language === 'en' "><img src= '../assets/img/greatbritain-flag.png' alt="greatbritain"></div>
-        <div v-else>`Bandiera non disponibile: Lang= {{details.original_language}}` </div>
+        <div v-if="details.original_language === 'it' "><img class="flags" src= '../assets/img/ita-flag.png' alt="italy"></div>
+        <div v-else-if="details.original_language === 'en' "><img class="flags" src= '../assets/img/greatbritain-flag.png' alt="greatbritain"></div>
+        <div v-else><img class="flags" src='../assets/img/arco-flag.png' alt="arcobaleno">Lang= {{details.original_language}} </div>
 
 
         <div>{{details.vote_average}}</div>
@@ -19,6 +20,9 @@ export default {
     name:"TvSeries",
     data() {
         return {
+            baseUrl: 'https://image.tmdb.org/t/p/',
+            sizeImg:'w342/',
+            posterPath: this.details.poster_path 
         }
     },
     props: {
@@ -35,7 +39,7 @@ export default {
 #tvseries {
     margin: 30px;
 }
-img {
+.flags{
     width: 20px;
 }
 
